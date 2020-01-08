bd0d9edf4c8306dac2974ad44a627937e25b89bd
Matthias Colin <matthias.colin@gmail.com>
2020-01-08 17:04:17 +0100

    end day 3

--- src/cinema/data/Movie.java
+++ src/cinema/data/Movie.java
@@ -2,7 +2,85 @@ package cinema.data;
 
 public class Movie {
 	// attributes
-	String title;
-	int year;
-	int duration;
+	private String title;
+	private int year;
+	private int duration;
+	
+	public Movie(String title, int year) {
+		this(title, year, 0);
+	}
+
+	public Movie(String title, int year, int duration) {
+		super();
+		this.title = title;
+		this.year = year;
+		this.duration = duration;
+	}
+
+	
+	
+	
+	public String getTitle() {
+		return title;
+	}
+
+
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	@Override
+	public String toString() {
+		return title + " (" + year 
+			//	+  ( duration==0 ? "" : ", " + duration + "mn")
+				+ ")";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + year;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (this.getClass() != obj.getClass())
+			return false;
+		Movie other = (Movie) obj;
+		if (this.title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!this.title.equals(other.title))
+			return false;
+		if (this.year != other.year)
+			return false;
+		return true;
+	}
+	
+	
 }
--- src/test/cinema/data/TestMovie.java
+++ src/test/cinema/data/TestMovie.java
@@ -2,6 +2,8 @@ package test.cinema.data;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import cinema.data.Movie;
@@ -9,10 +11,47 @@ import cinema.data.Movie;
 class TestMovie {
 
 	@Test
-	void test() {
-		Movie movie = new Movie();
+	void testFirst() {
+		Movie movie = new Movie("Joker", 2019, 165);
+		Movie movie2 = new Movie("Parasite", 2019, 132);
+		Movie movie3 = new Movie("Interstellar", 2014);
+		List<Movie> movies = List.of(movie, movie2, movie3);
+		System.out.println("Movies: " + movies); 
+		System.out.println("Title: " + movie.getTitle());
+		Movie oneMovie = movies.get(0);
+		System.out.println("Movie tonight: " + oneMovie);
+		// 
+		movie.setYear(1900);
 		System.out.println(movie);
-		new Movie();
+		System.out.println(movies.get(0));
+		System.out.println(oneMovie);
+	}
+	
+	@Test
+	void testEquals1() {
+		Movie movieJ = new Movie("Joker", 2019, 165);
+		Movie movieP = new Movie("Parasite", 2019, 132);
+		Movie movie = movieJ;
+		System.out.println(movieJ == movieP);
+		System.out.println(movieJ == movieJ);
+		System.out.println(movie == movieJ);
+	}
+	
+	
+	@Test
+	void testEquals() {
+		Movie movieChaosI = new Movie("Chaos", 2005);
+		Movie movieChaosII = new Movie("Chaos", 2005);
+		System.out.println(movieChaosI == movieChaosII);
+		System.out.println(movieChaosI.equals(movieChaosII));
+	}
+	
+	@Test
+	void testMovieAsObject() {
+		Movie movieChaosI = new Movie("Chaos", 2005);
+		Object obj = "Chaos";//movieChaosI;
+		Movie movie = (Movie) obj;
+		System.out.println(movie.getTitle());
 	}
 
 }
