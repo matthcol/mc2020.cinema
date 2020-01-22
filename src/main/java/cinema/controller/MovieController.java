4a0d6bb18dee89f093f0775a33ec0090d0aa51ca
Matthias Colin <matthias.colin@gmail.com>
2020-01-22 15:36:32 +0100

    add entries controller

--- src/main/java/cinema/controller/MovieController.java
+++ src/main/java/cinema/controller/MovieController.java
@@ -1,8 +1,53 @@
 package cinema.controller;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import cinema.persistence.entity.Movie;
+import cinema.persistence.repository.MovieRepository;
+
 @RestController
+@RequestMapping("/api/movie")
 public class MovieController {
+	
+	@Autowired
+	MovieRepository movieRepository;
+	
+	@GetMapping
+	@ResponseBody
+	public List<Movie> allMovies() {
+		return movieRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	@ResponseBody
+	public Optional<Movie> movieById(@PathVariable("id") int idMovie) {
+		return movieRepository.findById(idMovie);
+	}
+	
+	@GetMapping("/byTitle")
+	@ResponseBody
+	public Set<Movie> movieByPartialTitle(@RequestParam("t") String partialTitle) {
+		return movieRepository.findByTitleContainingIgnoreCase(partialTitle);
+	}
+	
+	@PostMapping
+	@ResponseBody
+	public Movie addMovie(@RequestBody Movie movie) {
+		Movie movieSaved = movieRepository.save(movie);
+		movieRepository.flush();
+		return movieSaved;
+	}
 
 }
