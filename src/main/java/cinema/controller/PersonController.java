41ab1a2b625a71f15db85bddfb75c6bd48835d4e
Matthias Colin <matthias.colin@gmail.com>
2020-01-24 14:01:55 +0100

    add get all person controller

--- src/main/java/cinema/controller/PersonController.java
+++ src/main/java/cinema/controller/PersonController.java
@@ -1,5 +1,16 @@
 package cinema.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import cinema.persistence.entity.Person;
+
 public class PersonController {
 
+	@GetMapping
+	List<Person> getAllPersons() {
+		// TODO
+		return null;
+	}
 }
