efd7ba1bf4e1540732122fdbfbd02a19fa197fb6
Matthias Colin <matthias.colin@gmail.com>
2020-01-24 13:58:01 +0100

    add post person on controller

--- src/main/java/cinema/controller/PersonController.java
+++ src/main/java/cinema/controller/PersonController.java
@@ -1,5 +1,16 @@
 package cinema.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+
+import cinema.persistence.entity.Person;
+
+
 public class PersonController {
+	
+	@PostMapping
+	Person addPerson(Person person) {
+		// TODO
+		return person;
+	}
 
 }
